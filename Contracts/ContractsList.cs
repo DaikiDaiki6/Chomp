dbfa378dbe5cef0a9472e527a76493b9c92cd0ac
DaikiDaiki6 <jeffreymdaqs@gmail.com>
2025-09-09 11:27:08 +0800

    Contracts value added - initial values

--- Contracts/ContractsList.cs
+++ Contracts/ContractsList.cs
@@ -1,15 +1,15 @@
 ﻿namespace Contracts;
 
-public record CreateUserEvent();
-public record UpdateUserEvent();
-public record DeleteUserEvent();
+public record CreateUserEvent(Guid UserId, string Username, string Email, string ContactNo, DateTime CreateAt);
+public record UpdateUserEvent(Guid UserId, string Username, string Email, string ContactNo, DateTime UpdatedAt);
+public record DeleteUserEvent(Guid UserId, string Username, string Email, string ContactNo, DateTime UpdatedAt); // not sure with UpdatedAt
 
-public record CreateOrderEvent();
-public record OrderSuccessEvent();
-public record OrderFailedEvent();
-public record OrderItem();
+public record CreateOrderEvent(Guid OrderId, Guid CustomerId, decimal TotalPrice, DateTime CreatedAt, List<OrderItem> OrderItems);
+public record OrderSuccessEvent(Guid OrderId, Guid CustomerId, decimal TotalPrice, DateTime CreatedAt, List<OrderItem> OrderItems);
+public record OrderFailedEvent(Guid OrderId, Guid CustomerId, string Reason);
+public record OrderItem(Guid OrderItemId, int Quantity, decimal UnitPrice, Guid ProductId, string ProductName, decimal TotalPrice);
 
-public record PaymentSuccessEvent();
-public record PaymentFailedEvent();
+public record PaymentSuccessEvent(Guid PaymentId, Guid CustomerId, Guid OrderId, string Status);
+public record PaymentFailedEvent(Guid PaymentId, Guid CustomerId, Guid OrderId, string Reason);
 
 
